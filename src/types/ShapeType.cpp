664bbae02e1b61f8b0f1d3e9af65f68cd015c69a
Dan Gezelter <gezelter@nd.edu>
2004-10-18 15:21:00 +0000

    Added ShapeType

---
+++ src/types/ShapeType.cpp
@@ -0,0 +1,53 @@
+#include "types/ShapeType.hpp"
+
+ShapeType::ShapeType(void) {
+  mass = 0.0;
+  I = new Matrix3x3d();
+  
+}
+
+ShapeType::~ShapeType(void) {
+}
+
+double ShapeType::getContactValueAt(double costheta, double phi) {
+
+  vector<RealSphericalHarmonic*>::iterator contactIter;
+  double contactVal;
+
+  contactVal = 0.0;
+
+  for(contactIter = contactFuncs.begin();  contactIter != contactFuncs.end(); 
+      ++contactIter)     
+    contactVal += (*contactIter)->getValueAt(costheta, phi);
+  
+  return contactVal;
+}
+
+double ShapeType::getRangeValueAt(double costheta, double phi) {
+  
+  vector<RealSphericalHarmonic*>::iterator rangeIter;
+  double rangeVal;
+  
+  rangeVal = 0.0;
+  
+  for(rangeIter = rangeFuncs.begin();  rangeIter != rangeFuncs.end(); 
+      ++rangeIter)     
+    rangeVal += (*rangeIter)->getValueAt(costheta, phi);
+  
+  return rangeVal;
+}
+
+double ShapeType::getStrengthValueAt(double costheta, double phi) {
+
+  vector<RealSphericalHarmonic*>::iterator strengthIter;
+  double strengthVal;
+
+  strengthVal = 0.0;
+
+  for(strengthIter = strengthFuncs.begin();  
+      strengthIter != strengthFuncs.end(); 
+      ++strengthIter)     
+    strengthVal += (*strengthIter)->getValueAt(costheta, phi);
+
+  return strengthVal;
+}
---
+++ src/types/ShapeType.hpp
@@ -0,0 +1,82 @@
+/**
+ * @file ShapeType.hpp
+ * @author Dan Gezelter
+ * @date 10/18/2004
+ * @version 1.0
+ */
+
+#ifndef TYPES_SHAPETYPE_HPP
+#define TYPES_SHAPETYPE_HPP
+
+#include <fstream>
+#include <vector>
+#include "math/RealSphericalHarmonic.hpp"
+#include "math/Matrix3x3d.hpp"
+
+namespace oopse {
+using namespace std;
+
+class ShapeType {
+
+public: 
+
+  ShapeType(void);
+  ~ShapeType(void);  
+
+  char *getName(void) {return shapeName;}
+  void setName(char * name) {shapeName = strdup(name);}
+
+  double getMass(void) {return mass;}
+  void setMass(double m) {mass = m;}
+
+  Matrix3x3d getI(void) {return I;}
+  void setI(SquareMatrix3 theI) {I = theI}
+
+  vector<RealSphericalHarmonic*> getContactFuncs(void) {return contactFuncs;}
+  vector<RealSphericalHarmonic*> getRangeFuncs(void) {return rangeFuncs;}
+  vector<RealSphericalHarmonic*> getStrengthFuncs(void) {return strengthFuncs;}
+
+  void setContactFuncs(vector<RealSphericalHarmonic*> cf) {
+    contactFuncs = cf;
+  }
+  void setRangeFuncs(vector<RealSphericalHarmonic*> rf) {
+    rangeFuncs = rf;
+  }
+  void setStrengthFuncs(vector<RealSphericalHarmonic*> sf) {
+    strengthFuncs = sf;
+  }
+
+  /**
+   * Gets the value of the contact function at a particular orientation
+   * @param costheta
+   * @param phi
+   */
+  double getContactValueAt(double costheta, double phi);
+
+  /**
+   * Gets the value of the range function at a particular orientation
+   * @param costheta
+   * @param phi
+   */
+  double getRangeValueAt(double costheta, double phi);
+
+  /**
+   * Gets the value of the strength function at a particular orientation
+   * @param costheta
+   * @param phi
+   */
+  double getStrengthValueAt(double costheta, double phi);
+
+private:
+
+  char *shapeName;             // The name of the shape
+  double mass;                 // The mass
+  Matrix3x3d I;                // The moment of inertia tensor
+  vector<RealSphericalHarmonic*> contactFuncs;  // The contact functions
+  vector<RealSphericalHarmonic*> rangeFuncs;    // The range functions
+  vector<RealSphericalHarmonic*> strengthFuncs; // The strength functions
+  
+};
+
+#endif
+
