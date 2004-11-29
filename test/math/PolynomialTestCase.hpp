86a07d0bebfbfc84e1880c6820c097da3f0c17b4
Teng Lin <teng.lin@gmail.com>
2004-11-29 21:42:52 +0000

    add PolynomialTestCase

---
+++ test/math/PolynomialTestCase.hpp
@@ -0,0 +1,27 @@
+#ifndef TEST_POLYNOMIALTESTCASE_HPP
+#define TEST_POLYNOMIALTESTCASE_HPP
+
+#include <cppunit/extensions/HelperMacros.h>
+#include "math/Polynomial.hpp"
+ 
+using namespace oopse;
+
+class PolynomialTestCase : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE( PolynomialTestCase );
+    CPPUNIT_TEST(testPolynomial);
+
+    CPPUNIT_TEST_SUITE_END();
+
+    public:
+
+        void testPolynomial();
+        
+    private:
+
+        
+};
+
+
+#endif //TEST_POLYNOMIALTESTCASE_HPP
+
+
