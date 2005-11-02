89ea254f21003a5134772a7de9f7f17d2a57bf13
Christopher Fennell <cfennell@gmail.com>
2005-11-02 20:45:30 +0000

    forgot to add the screening method header

---
+++ src/UseTheForce/DarkSide/fScreeningMethod.h
@@ -0,0 +1,16 @@
+#ifdef __C
+#ifndef __FSCREENINGMETHOD
+#define __FSCREENINGMETHOD
+
+#define UNDAMPED               1
+#define DAMPED                 2
+
+#endif
+#endif /*__C*/
+
+#ifdef  __FORTRAN90
+
+  INTEGER, PARAMETER:: UNDAMPED              = 1
+  INTEGER, PARAMETER:: DAMPED                = 2
+
+#endif
