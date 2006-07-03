8f81af9302dde377f3e6b0136113b57e695a99df
Charles Vardeman <charles.vardeman@gmail.com>
2006-07-03 19:40:52 +0000

    Added utility function to winger3jm from slatac.

--- src/math/Wigner3jm.F90
+++ src/math/Wigner3jm.F90
@@ -1,6 +1,6 @@
 subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
      IER)
-  use status
+ ! use status
 !
 !! DRC3JM evaluates the 3j symbol g(M2) for all allowed values of M2.
 !
@@ -158,18 +158,21 @@ subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
   if ( (L1-ABS(M1)+EPS < ZERO).OR. &
      (MOD(L1+ABS(M1)+EPS,ONE) >= EPS+EPS))THEN
      IER=1
-     call handleError('Wigner3jm','L1-ABS(M1) less than zero or '// &
-        'L1+ABS(M1) not integer.')
+!     call handleError('Wigner3jm','L1-ABS(M1) less than zero or '// &
+!        'L1+ABS(M1) not integer.')
      return
   ELSEIF((L1+L2-L3 < -EPS).OR.(L1-L2+L3 < -EPS).OR. &
      (-L1+L2+L3 < -EPS))THEN
      IER=2
-     call handleError('Wigner3jm','L1, L2, L3 do not satisfy '// &
-        'triangular condition.')
+     write(*,*) eps
+     write(*,*) L1,L2,L3
+     write(*,*) "L1,L2,L3 do not satisfy triangular condition"
+!     call handleError('Wigner3jm','L1, L2, L3 do not satisfy '// &
+!        'triangular condition.')
      return
   ELSEIF(MOD(L1+L2+L3+EPS,ONE) >= EPS+EPS)THEN
      IER=3
-     call handleError('Wigner3jm','L1+L2+L3 not integer.')
+!     call handleError('Wigner3jm','L1+L2+L3 not integer.')
      return
   end if
 !
@@ -181,7 +184,7 @@ subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
 !  Check error condition 4.
   if ( MOD(M2MAX-M2MIN+EPS,ONE) >= EPS+EPS)THEN
      IER=4
-     call handleError('Wigner3jm','M2MAX-M2MIN not integer.')
+!     call handleError('Wigner3jm','M2MAX-M2MIN not integer.')
      return
   end if
   if ( M2MIN < M2MAX-EPS)   go to 20
@@ -189,7 +192,7 @@ subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
 !
 !  Check error condition 5.
   IER=5
-  call handleError('Wigner3jm','M2MIN greater than M2MAX.')
+!  call handleError('Wigner3jm','M2MIN greater than M2MAX.')
   return
 !
 !
@@ -208,8 +211,8 @@ subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
 !
 !  Check error condition 6.
    21 IER = 6
-  call handleError('Wigner3jm','Dimension of result array for '// &
-              '3j coefficients too small.')
+!  call handleError('Wigner3jm','Dimension of result array for '// &
+!              '3j coefficients too small.')
   return
 !
 !
@@ -424,3 +427,82 @@ subroutine WIGNER3JM (L1, L2, L3, M1, M2MIN, M2MAX, THRCOF, NDIM, &
 !
   return
 end
+
+
+!DECK D1MACH
+      DOUBLE PRECISION FUNCTION D1MACH (I)
+      IMPLICIT NONE
+      INTEGER :: I
+      DOUBLE PRECISION :: B, X
+!***BEGIN PROLOGUE  D1MACH
+!***PURPOSE  Return floating point machine dependent constants.
+!***LIBRARY   SLATEC
+!***CATEGORY  R1
+!***TYPE      SINGLE PRECISION (D1MACH-S, D1MACH-D)
+!***KEYWORDS  MACHINE CONSTANTS
+!***AUTHOR  Fox, P. A., (Bell Labs)
+!           Hall, A. D., (Bell Labs)
+!           Schryer, N. L., (Bell Labs)
+!***DESCRIPTION
+!
+!   D1MACH can be used to obtain machine-dependent parameters for the
+!   local machine environment.  It is a function subprogram with one
+!   (input) argument, and can be referenced as follows:
+!
+!        A = D1MACH(I)
+!
+!   where I=1,...,5.  The (output) value of A above is determined by
+!   the (input) value of I.  The results for various values of I are
+!   discussed below.
+!
+!   D1MACH(1) = B**(EMIN-1), the smallest positive magnitude.
+!   D1MACH(2) = B**EMAX*(1 - B**(-T)), the largest magnitude.
+!   D1MACH(3) = B**(-T), the smallest relative spacing.
+!   D1MACH(4) = B**(1-T), the largest relative spacing.
+!   D1MACH(5) = LOG10(B)
+!
+!   Assume single precision numbers are represented in the T-digit,
+!   base-B form
+!
+!              sign (B**E)*( (X(1)/B) + ... + (X(T)/B**T) )
+!
+!   where 0 .LE. X(I) .LT. B for I=1,...,T, 0 .LT. X(1), and
+!   EMIN .LE. E .LE. EMAX.
+!
+!   The values of B, T, EMIN and EMAX are provided in I1MACH as
+!   follows:
+!   I1MACH(10) = B, the base.
+!   I1MACH(11) = T, the number of base-B digits.
+!   I1MACH(12) = EMIN, the smallest exponent E.
+!   I1MACH(13) = EMAX, the largest exponent E.
+!
+!
+!***REFERENCES  P. A. Fox, A. D. Hall and N. L. Schryer, Framework for
+!                 a portable library, ACM Transactions on Mathematical
+!                 Software 4, 2 (June 1978), pp. 177-188.
+!***ROUTINES CALLED  XERMSG
+!***REVISION HISTORY  (YYMMDD)
+!   790101  DATE WRITTEN
+!   960329  Modified for Fortran 90 (BE after suggestions by EHG)      
+!***END PROLOGUE  D1MACH
+!      
+      X = 1.0D0
+      B = RADIX(X)
+      SELECT CASE (I)
+        CASE (1)
+          D1MACH = B**(MINEXPONENT(X)-1) ! the smallest positive magnitude.
+        CASE (2)
+          D1MACH = HUGE(X)               ! the largest magnitude.
+        CASE (3)
+          D1MACH = B**(-DIGITS(X))       ! the smallest relative spacing.
+        CASE (4)
+          D1MACH = B**(1-DIGITS(X))      ! the largest relative spacing.
+        CASE (5)
+          D1MACH = LOG10(B)
+        CASE DEFAULT
+          WRITE (*, FMT = 9000)
+ 9000     FORMAT ('1ERROR    1 IN D1MACH - I OUT OF BOUNDS')
+          STOP
+      END SELECT
+      RETURN
+      END
