5fbfac95790994be83d104766feaca5622ea278c
Dan Gezelter <gezelter@nd.edu>
2013-02-20 15:39:39 +0000

    Fixed a widespread typo in the license

--- src/applications/openmd/openmd.cpp
+++ src/applications/openmd/openmd.cpp
@@ -35,7 +35,7 @@
  *                                                                      
  * [1]  Meineke, et al., J. Comp. Chem. 26, 252-271 (2005).             
  * [2]  Fennell & Gezelter, J. Chem. Phys. 124, 234104 (2006).          
- * [3]  Sun, Lin & Gezelter, J. Chem. Phys. 128, 24107 (2008).          
+ * [3]  Sun, Lin & Gezelter, J. Chem. Phys. 128, 234107 (2008).          
  * [4]  Kuang & Gezelter,  J. Chem. Phys. 133, 164101 (2010).
  * [5]  Vardeman, Stocker & Gezelter, J. Chem. Theory Comput. 7, 834 (2011).
  * [6]  Kuang & Gezelter, Mol. Phys., 110, 691-701 (2012).
@@ -115,7 +115,7 @@ int main(int argc, char* argv[]){
       "  |                                                                          |\n"<<
       "  | [1] Meineke, et al., J. Comp. Chem. 26, 252-271 (2005).                  |\n"<<
       "  | [2] Fennell & Gezelter, J. Chem. Phys. 124, 234104 (2006).               |\n"<<
-      "  | [3] Sun, Lin & Gezelter, J. Chem. Phys. 128, 24107 (2008).               |\n"<<
+      "  | [3] Sun, Lin & Gezelter, J. Chem. Phys. 128, 234107 (2008).               |\n"<<
       "  | [4] Kuang & Gezelter,  J. Chem. Phys. 133, 164101 (2010).                |\n"<<
       "  | [5] Vardeman, Stocker & Gezelter, J. Chem. Theory Comput. 7, 834 (2011). |\n"<<
       "  | [6] Kuang & Gezelter, Mol. Phys., 110, 691-701 (2012).                   |\n"<<
--- src/applications/staticProps/TetrahedralityParamZ.cpp
+++ src/applications/staticProps/TetrahedralityParamZ.cpp
@@ -35,7 +35,7 @@
  *                                                                   
  * [1]  Meineke, et al., J. Comp. Chem. 26, 252-271 (2005).             
  * [2]  Fennell & Gezelter, J. Chem. Phys. 124, 234104 (2006).          
- * [3]  Sun, Lin & Gezelter, J. Chem. Phys. 128, 24107 (2008).          
+ * [3]  Sun, Lin & Gezelter, J. Chem. Phys. 128, 234107 (2008).          
  * [4]  Kuang & Gezelter,  J. Chem. Phys. 133, 164101 (2010).
  * [5]  Vardeman, Stocker & Gezelter, J. Chem. Theory Comput. 7, 834 (2011).
  * [6]  Kuang & Gezelter, Mol. Phys., 110, 691-701 (2012).
