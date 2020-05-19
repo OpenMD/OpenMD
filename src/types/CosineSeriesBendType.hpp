df6c9f44223ff27a77d0df4c2afa16baebf4e767
Dan Gezelter <gezelter@nd.edu>
2020-05-19 16:58:55 -0400

    Adding CosineSeries Bend type for use with UFF

---
+++ src/types/CosineSeriesBendType.hpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2005 The University of Notre Dame. All Rights Reserved.
+ *
+ * The University of Notre Dame grants you ("Licensee") a
+ * non-exclusive, royalty free, license to use, modify and
+ * redistribute this software in source and binary code form, provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. All express or implied conditions, representations and
+ * warranties, including any implied warranty of merchantability,
+ * fitness for a particular purpose or non-infringement, are hereby
+ * excluded.  The University of Notre Dame and its licensors shall not
+ * be liable for any damages suffered by licensee as a result of
+ * using, modifying or distributing the software or its
+ * derivatives. In no event will the University of Notre Dame or its
+ * licensors be liable for any lost revenue, profit or data, or for
+ * direct, indirect, special, consequential, incidental or punitive
+ * damages, however caused and regardless of the theory of liability,
+ * arising out of the use of or inability to use software, even if the
+ * University of Notre Dame has been advised of the possibility of
+ * such damages.
+ *
+ * SUPPORT OPEN SCIENCE!  If you use OpenMD or its source code in your
+ * research, please cite the appropriate papers when you publish your
+ * work.  Good starting points are:
+ *                                                                      
+ * [1]  Meineke, et al., J. Comp. Chem. 26, 252-271 (2005).             
+ * [2]  Fennell & Gezelter, J. Chem. Phys. 124, 234104 (2006).          
+ * [3]  Sun, Lin & Gezelter, J. Chem. Phys. 128, 234107 (2008).          
+ * [4]  Kuang & Gezelter,  J. Chem. Phys. 133, 164101 (2010).
+ * [5]  Vardeman, Stocker & Gezelter, J. Chem. Theory Comput. 7, 834 (2011).
+ */
+ 
+#ifndef TYPES_COSINESERIESBENDTYPE_HPP
+#define TYPES_COSINESERIESBENDTYPE_HPP
+
+#include "types/BendType.hpp"
+
+namespace OpenMD {
+  /**
+   * @class CosineSeriesBendType 
+   *
+   * A bend used primarily in UFF that utilizes a Fourier series in
+   * terms of the cosine of the bend angle:
+   *
+   * \f[ Vbend = k \left[ C_0 + C_1 \cos(\theta) + C_2 \cos(2 \theta) \right] \f]
+   */
+  class CosineSeriesBendType : public BendType {
+    
+  public:
+    
+    CosineSeriesBendType(RealType theta, RealType k) : BendType(theta), k_(k) {
+      RealType st = sin(theta);
+      RealType ct = cos(theta);
+      c2_ = 1.0 / (4.0 * st*st);
+      c1_ = -4.0 * c2_ * ct;
+      c0_ = c2_ * (2.0 * ct * ct + 1.0);     
+    }
+    
+    void setForceConstant(RealType k) {k_ = k; }
+    
+    RealType getForceConstant() {return k_;}
+    
+    void calcForce(RealType theta, RealType& V, RealType& dVdtheta) {
+
+      RealType ct = cos(theta);
+      RealType st = sin(theta);    
+      RealType delta = ct - c0_;
+
+      // use cos 2t = (2 cos^2 - 1)
+      V =  k_ * (c0_ + c1_ * ct + c2_ * (2.0 * ct * ct - 1.0)); 
+      dVdtheta = - k_ * (c1_ * st + 2.0 * c2_ * sin(2.0 * theta));
+    }
+    
+  private:
+    RealType k_;
+    RealType c0_;
+    RealType c1_;
+    RealType c2_;
+    
+  };
+  
+}//end namespace OpenMD
+#endif //TYPES_COSINESERIESBENDTYPE_HPP
+
