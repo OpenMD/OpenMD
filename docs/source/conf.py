f5feb4aeeeaf37d56ed14c7b24dab7b11e23fb57
Dan Gezelter <gezelter@nd.edu>
2022-11-17 16:21:33 -0500

    One more update for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OpenMD'
+copyright = '2022, Gezelter'
+author = 'Gezelter'
 
-release = '0.1'
-version = '0.1.0'
+release = '2.7'
+version = '2.7'
 
 # -- General configuration
 
