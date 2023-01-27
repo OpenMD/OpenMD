d3ac893f9de36c4ad24d7c5d907b07d3bf0cf819
crdrisko <crdrisko@fastmail.com>
2023-01-27 12:21:56 -0700

    Updates to all the copyright headers before pull request

--- src/types/ConstraintStamp.hpp
+++ src/types/ConstraintStamp.hpp
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2004-2022, The University of Notre Dame. All rights reserved.
+ * Copyright (c) 2004-present, The University of Notre Dame. All rights
+ * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
+ *    this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
